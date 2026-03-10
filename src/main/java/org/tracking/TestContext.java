82dcaa6767de758f5d7c1e47b317824b5147aeff
Zeno0703 <zeno.vandenbulcke456@gmail.com>
2026-03-10 21:01:21 +0100

    fix: make sub-thread inherit name

--- src/main/java/org/tracking/TestContext.java
+++ src/main/java/org/tracking/TestContext.java
@@ -4,17 +4,22 @@ import org.utils.StringUtils;
 
 public class TestContext {
 
-    private static final ThreadLocal<String> current = new ThreadLocal<>();
+    private static final ThreadLocal<String> current = new InheritableThreadLocal<>();
+    private static volatile String globalFallback = null;
 
     public static void enter(String id) {
-        current.set(StringUtils.sanitize(id));
+        String sanitized = StringUtils.sanitize(id);
+        current.set(sanitized);
+        globalFallback = sanitized;
     }
 
     public static String getCurrent() {
-        return current.get();
+        String local = current.get();
+        return local != null ? local : globalFallback;
     }
 
     public static void exit() {
         current.remove();
+        globalFallback = null;
     }
 }
